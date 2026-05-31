6ae5ccf9d5df6167e4124fbd4a98803458232706
Анатолий Журавлев <azhu@1c.ru>
2026-05-31 13:32:44 +0300

    Исправление представления внешних ссылок в тексте

--- src/modules/markdown.ts
+++ src/modules/markdown.ts
@@ -13,45 +13,53 @@ export class Markdown {
     }
 
     async fix(text: string): Promise<string> {
-        // Fix ссылок
-        text = text.replace(/\[([^[]+?)\|([^]+?)]/g, (match, link, title) => (
-            `[${title}](${!link.startsWith(LINK_PREFIX) ? LINK_PREFIX : ''}${link})`
-        ));
+    // Fix внешних ссылок VK вида:
+    // [#alias|example.com/index...|https://example.com/index...]
+    text = text.replace(/\[#alias\|([^|\]]+)\|([^\]]+)]/g, (match, title, link) => {
+        return `[${title}](${link})`;
+    });
 
-        // Fix хештегов
-        text = await replaceAsync(text, /(?:^|\s)#([^\s]+)/g, async (match, hashtag): Promise<string> => {
-            const space = match.startsWith('\n') ?
-                '\n'
+    // Fix обычных VK-ссылок вида:
+    // [club123|Название]
+    // [id123|Имя]
+    text = text.replace(/\[([^|\[\]]+)\|([^\]]+)]/g, (match, link, title) => {
+        return `[${title}](${!link.startsWith(LINK_PREFIX) ? LINK_PREFIX : ''}${link})`;
+    });
+
+    // Fix хештегов
+    text = await replaceAsync(text, /(?:^|\s)#([^\s]+)/g, async (match, hashtag): Promise<string> => {
+        const space = match.startsWith('\n') ?
+            '\n'
+            :
+            match.startsWith(' ') ?
+                ' '
                 :
-                match.startsWith(' ') ?
-                    ' '
-                    :
-                    '';
+                '';
 
-            const isNavigationHashtag = match.match(/#([^\s]+)@([a-zA-Z_\d]+)/);
+        const isNavigationHashtag = match.match(/#([^\s]+)@([a-zA-Z_\d]+)/);
 
-            if (isNavigationHashtag) {
-                const [, hashtag, group] = isNavigationHashtag;
+        if (isNavigationHashtag) {
+            const [, hashtag, group] = isNavigationHashtag;
 
-                const resource = await this.VK.resolveResource(group)
-                    .catch(() => null);
+            const resource = await this.VK.resolveResource(group)
+                .catch(() => null);
 
-                if (resource?.type === 'group') {
-                    if (hashtag.match(/[a-zA-Z]+/)) {
-                        return `${space}[#${hashtag}@${group}](${LINK_PREFIX}${group}/${hashtag})`;
-                    }
-
-                    return `${space}[#${hashtag}@${group}](${LINK_PREFIX}wall-${resource.id}?q=%23${hashtag})`;
+            if (resource?.type === 'group') {
+                if (hashtag.match(/[a-zA-Z]+/)) {
+                    return `${space}[#${hashtag}@${group}](${LINK_PREFIX}${group}/${hashtag})`;
                 }
+
+                return `${space}[#${hashtag}@${group}](${LINK_PREFIX}wall-${resource.id}?q=%23${hashtag})`;
             }
-
-            return `${space}[#${hashtag}](${LINK_PREFIX}feed?section=search&q=%23${hashtag})`;
-        });
-
-        try {
-            return decodeURI(text);
-        } catch {
-            return text;
         }
+
+        return `${space}[#${hashtag}](${LINK_PREFIX}feed?section=search&q=%23${hashtag})`;
+    });
+
+    try {
+        return decodeURI(text);
+    } catch {
+        return text;
     }
+}
 }
